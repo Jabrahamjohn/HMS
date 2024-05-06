22d5642ba3ca02279a61183fef5d54da01e5cc77
Abraham John <jabrahamjohns@gmail.com>
2024-05-07 00:39:41 +0300

    backend

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User'); // Import the User model
+
+// Controller function to list users
+exports.listUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Controller function to create a new user
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Add more functions as needed for updating and deleting users
